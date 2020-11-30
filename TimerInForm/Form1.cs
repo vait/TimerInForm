64426608de864f9da9120ecdbbe47e3c84a6c393
Victor V. Grekov <victor.grek@gmail.com>
2020-11-30 22:51:01 +0300

    the first commit

---
+++ TimerInForm/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TimerInForm
+{
+	public partial class Form1 : Form
+	{
+		private readonly SafeTimerDecorator _timer;
+
+		public Form1()
+		{
+			InitializeComponent();
+			_timer = new SafeTimerDecorator(ShowTime, null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_timer.Change(TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			_timer.Stop();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			_timer?.Dispose();
+		}
+
+		private void ShowTime(object state)
+		{
+			Invoke((MethodInvoker)(() =>
+			{
+				label1.Text = DateTime.Now.ToString("HH:mm:ss.fff tt");
+
+			}));
+		}
+
+
+	}
+}
---
+++ TimerInForm/SafeTimerDecorator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerInForm
+{
+	class SafeTimerDecorator : IDisposable
+	{
+		private readonly Timer _timer;
+
+		/// <inheritdoc cref="Timer"/>
+		public SafeTimerDecorator(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+		{
+			_timer = new Timer(RunSafe(callback), state, dueTime, period);
+		}
+
+		public bool Change(TimeSpan dueTime, TimeSpan period)
+		{
+			return _timer.Change(dueTime, period);
+		}
+
+		public void Stop()
+		{
+			Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+		}
+
+		private TimerCallback RunSafe(TimerCallback action)
+		{
+			return state =>
+			{
+				try
+				{
+					action(state);
+				}
+				catch (Exception ex)
+				{
+					//Do logging
+				}
+			};
+		}
+
+		public void Dispose()
+		{
+			_timer.Dispose();
+		}
+	}
+}
